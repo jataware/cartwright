c547bbb9096df48d73f3e14982c15562c55d94b8
David Samson <david.andrew.engineer@gmail.com>
2022-05-18 16:29:32 -0400

    pushing (failed) attempt to get one class classifier to work

--- geotime_classify/training_model/oc-nn.py
+++ geotime_classify/training_model/oc-nn.py
@@ -11,12 +11,23 @@ import pdb
 
 
 def main():
-    normal_train, normal_test, anomaly = get_data()
-    
+    train_model()
+
+
+class OCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.V = nn.Linear(784, 256)
+        self.w = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = F.relu(self.V(x))
+        x = self.w(x)
+        return x
 
 
 
-def get_data(normal_class=6):
+def get_data(normal_class=6, device='cuda'):
     """get mnist data split into train and test for the normal class, and all others are anomalies"""
 
     mnist = datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
@@ -45,9 +56,12 @@ def get_data(normal_class=6):
     normal_test /= 255.0
     anomaly /= 255.0
 
+    # move the data to the specified device
+    normal_train = normal_train.to(device)
+    normal_test = normal_test.to(device)
+    anomaly = anomaly.to(device)
+
     return normal_train, normal_test, anomaly
-
-
 
 
 def vec2img(vector: torch.Tensor) -> torch.Tensor:
@@ -61,6 +75,42 @@ def vecshow(vector: torch.Tensor) -> None:
     plt.show()
 
 
+def train_model():
+    #get data and move to the GPU
+    normal_train, normal_test, anomaly = get_data(device='cuda')
+
+    #create the model and move to the GPU
+    model = OCNN().cuda()
+    # r = 1.0 #radius from the origin
+    nu = 0.1 #tradeoff between points crossing hyperplane and hyperplane distance from origin
+    N = normal_train.shape[0]
+
+    #create the optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    #training loop
+    for epoch in range(100):
+        print(f'----------------- epoch {epoch} -----------------')
+        
+        for i in range(100):
+            optimizer.zero_grad()
+            y_hat = model(normal_train)
+            r = torch.quantile(y_hat, nu)
+            model_loss = F.relu(r - y_hat).sum() / N / nu
+            param_loss = (model.V.weight ** 2).sum()
+            loss = model_loss + param_loss
+            loss.backward()
+            optimizer.step()
+
+            #set r to the nu'th quantile of y_hat
+
+            print(f'{loss.item():.4f}')
+
+        pdb.set_trace()
+
+
+
+        # pdb.set_trace()
 
 
 if __name__ == '__main__':
