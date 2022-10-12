cec2a110e3a90f48ee24ca1986a23b8ab1cebf01
David Samson <david.andrew.engineer@gmail.com>
2022-10-12 17:01:08 -0400

    updated classification process to include data on temporal resolution of detected time columns

--- geotime_classify/geotime_classify.py
+++ geotime_classify/geotime_classify.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals, print_function, division
+from geotime_classify import utilities
 
 import torch
 import torch.autograd as autograd
@@ -2680,6 +2681,23 @@ class GeoTimeClassify:
 
         return index_nan
 
+    def predict_temporal_resolution(self, df:pd.DataFrame, final: geotime_schema.Classifications) -> geotime_schema.Classifications:
+        for classification in final.classifications:
+            
+            if classification.category != geotime_schema.category.time:
+                continue
+            if classification.format is None:
+                continue
+
+            #convert the datetime strings in the dataframe to unix timestamps using the classification format
+            times = df[classification.column].to_list()
+            times = [datetime.datetime.strptime(s, classification.format).timestamp() for s in times]
+            times = np.array(times)
+            
+            classification.time_resolution = utilities.detect_resolution(times)
+
+        return final
+    
     def columns_classified(self, path):
         logging.info('starting classification')
         df = self.read_in_csv(path)
@@ -2690,6 +2708,7 @@ class GeoTimeClassify:
         output = self.assign_heuristic_function_enhanced(preds, fuzzyMatchColumns)
         fuzzyMatch = self.fuzzymatchColumns(output)
         final = self.final_step(fuzzyMatch)
+        final = self.predict_temporal_resolution(df, final)
         return final
 
     def get_Fake_Data(self):
---
+++ geotime_classify/utilities.py
@@ -0,0 +1,55 @@
+import numpy as np
+from typing import Optional
+from geotime_classify.geotime_schema import TimeUnits, Uniformity, TimeResolution
+
+def detect_resolution(times:np.ndarray) -> Optional[TimeResolution]:
+    """
+    Detect the resolution of temporal data.
+    
+    @param times: a numpy array of unix times in SECONDS (may have duplicates)
+    @return: (optional) TimeResolution(uniformity, unit_name, avg_density, avg_error) where 
+        - uniformity is a Uniformity enum 
+        - unit_name is a TimeUnits enum
+        - avg_density is the median density of the data in (unit_name) units
+        - avg_error is the mean error in (unit_name) units   
+    """
+
+    #get all the deltas between each (unique) time
+    times = np.unique(times)
+    if len(times) < 2:
+        print('error: not enough timestamps to determine resolution')
+        return None
+    times.sort()
+    deltas = times[1:] - times[:-1]
+
+    #compute the average delta
+    # avg = stats.mode(deltas)[0][0]
+    # avg = np.mean(deltas)
+    avg = np.median(deltas)
+
+    #if all data within 1% of the mode, assume uniform
+    uniformity_score = np.abs(deltas - avg)
+    if np.all(uniformity_score == 0.0):
+        print('perfectly uniform')
+        uniformity = Uniformity.PERFECT
+    elif uniformity_score.max() < 0.01 * avg:
+        print('uniform to within 1%')
+        uniformity = Uniformity.UNIFORM
+    else:
+        print('not uniform')
+        uniformity = Uniformity.NOT_UNIFORM
+
+    #find the closest duration unit
+    names = [*TimeUnits.__members__.keys()]
+    durations = np.array([getattr(TimeUnits, name).value for name in names])
+    unit_errors = np.abs(durations - avg)/durations
+    closest = np.argmin(unit_errors)
+    errors = np.abs(1 - deltas / durations[closest]) #errors in terms of the closest unit
+
+    #TODO: could do some sort of thresholding to determine if it matched any of the durations or was something else
+    # print(f'`{names[closest]}` matches average delta with {unit_errors[closest]*100:.2f}% error')
+    # print(f'all deltas ({names[closest]}s):')
+    # print(f'{deltas/durations[closest]}')
+    # print('=========================================')
+
+    return TimeResolution(uniformity, names[closest], avg/durations[closest], errors.mean())
