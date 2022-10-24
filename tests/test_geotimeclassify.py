e25a7965abe8974de1bc62cfbee29cbdfd1b46e9
David Samson <david.andrew.engineer@gmail.com>
2022-10-24 11:57:42 -0400

    started integrating pytest and tox for automated testing. another restructure of tests file

--- tests/test_geotimeclassify.py
+++ tests/test_geotimeclassify.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 from geotime_classify import geotime_classify
+from geotime_classify.geotime_schema import TimeUnit, Uniformity
 import os
 import numpy as np
 import pandas as pd
-import timeit
-import time
-from typing import Callable, Generator, Sequence, Iterator, Tuple, List, Union
+from typing import Callable, Iterator, Tuple, Union
+import pytest
+
 
 import pdb
 
-from geotime_classify.geotime_schema import TimeUnit, Uniformity
 
 #disable logging for tests
 import logging
@@ -20,48 +20,9 @@ np.random.seed(0)
 
 
 
-TestGen = Iterator[Union[Tuple[str, (Callable[[], None])], Callable[[], None]]]
-
-
-
-def main():
-    #functions that generate suites of tests    
-    test_generators: List[Callable[[], TestGen]] = [
-        time_resolution_algorithm,
-        time_resolution_whole_pipeline,
-        #### insert more test generators here ####
-    ]
-
-    total_passed = 0
-    total_failed = 0
-    for test_generator in test_generators:
-        print(f'---------------------{test_generator.__name__}---------------------')
-        passed = 0
-        failed = 0
-        for test in test_generator():
-            if isinstance(test, tuple):
-                name, test = test
-            else:
-                name = test.__name__
-            try:
-                t = timeit.Timer(test)
-                print(f'PASSED {name}. runtime: {t.timeit(1):.04f} (seconds)')
-                passed += 1
-            except Exception as e:
-                print(f'FAILED {name}: {e}')
-                failed += 1
-        
-        print(f'Completed tests for `{test_generator.__name__}`')
-        print(f'PASSED {passed} tests, FAILED {failed} tests')
-        total_passed += passed
-        total_failed += failed
-
-    print(f'---------------------------------SUMMARY---------------------------------')
-    print('Completed all test suites')
-    print(f'PASSED {total_passed} tests, FAILED {total_failed} tests')
-
-
-
+"""
+Run tests with pytest in the root directory of the project
+"""
 
 
 # def testoncsv():
@@ -76,97 +37,98 @@ def main():
 #     assert time_res.unit == TimeUnit.year
 
 
+@pytest.mark.parametrize("unit,uniformity", 
+    [
+        pytest.param(TimeUnit.millisecond, Uniformity.PERFECT, marks=pytest.mark.xfail(reason="milliseconds has precision errors")),
+        pytest.param(TimeUnit.millisecond, Uniformity.UNIFORM, marks=pytest.mark.xfail(reason="milliseconds has precision errors")),
+        pytest.param(TimeUnit.millisecond, Uniformity.NOT_UNIFORM),
+        *[(unit, uniformity) for uniformity in Uniformity for unit in TimeUnit if unit != TimeUnit.millisecond],
+    ]
+)
+def test_time_resolution_algorithm(unit:TimeUnit, uniformity:Uniformity, num_rows=int(1e6)):
 
-def time_resolution_algorithm() -> TestGen:
-    def time_resolution_algorithm(unit:TimeUnit, uniformity:Uniformity, num_rows=int(1e6)):
+    #generate some fake data
+    times = np.ones(num_rows,dtype=np.float64) * unit
+    times = times.cumsum()
+    times += np.random.randint(-377711962054, 379654882584) #10,000 BCE - 14,000 CE
 
-        #generate some fake data
-        times = np.ones(num_rows,dtype=np.float64) * unit
-        times = times.cumsum()
-        times += np.random.randint(-377711962054, 379654882584) #10,000 BCE - 14,000 CE
+    if uniformity == Uniformity.PERFECT:
+        pass
+    elif uniformity == Uniformity.UNIFORM:
+        times += np.random.uniform(-0.004,0.004,num_rows)*unit
+    elif uniformity == Uniformity.NOT_UNIFORM:
+        times += np.random.uniform(-0.1,0.1,num_rows)*unit
 
-        if uniformity == Uniformity.PERFECT:
-            pass
-        elif uniformity == Uniformity.UNIFORM:
-            times += np.random.uniform(-0.0045,0.0045,num_rows)*unit
-        elif uniformity == Uniformity.NOT_UNIFORM:
-            times += np.random.uniform(-0.1,0.1,num_rows)*unit
+    #run the test
+    res = geotime_classify.time_resolution.detect_resolution(times)
+    assert res.unit == unit, f'failed to detect {unit}, instead got {res.unit}'
+    assert res.uniformity == uniformity, f'failed to detect {uniformity} uniformity for {unit}, instead got {res.uniformity}'
 
-        #run the test
-        res = geotime_classify.time_resolution.detect_resolution(times)
-        # print(res)
-        assert res.unit == unit, f'failed to detect {unit}, instead got {res.unit}'
-        assert res.uniformity == uniformity, f'failed to detect {uniformity} uniformity for {unit}, instead got {res.uniformity}'
 
-    #generator for the test suite
-    for uniformity in Uniformity:
-        for unit in [*TimeUnit][1:]: #skip milliseconds b/c precision isn't high enough
-            yield f'time_resolution_algorithm({unit},{uniformity})', lambda: time_resolution_algorithm(unit, uniformity)
+@pytest.mark.parametrize("unit,uniformity", 
+    [
+        pytest.param(TimeUnit.millisecond, Uniformity.PERFECT, marks=pytest.mark.xfail(reason="milliseconds has precision errors")),
+        pytest.param(TimeUnit.millisecond, Uniformity.UNIFORM, marks=pytest.mark.xfail(reason="milliseconds has precision errors")),
+        pytest.param(TimeUnit.millisecond, Uniformity.NOT_UNIFORM, marks=pytest.mark.xfail(reason="seconds is detected here for some reason")),
+        pytest.param(TimeUnit.second, Uniformity.PERFECT),
+        pytest.param(TimeUnit.second, Uniformity.UNIFORM, marks=pytest.mark.xfail(reason="for some reason, uniformity is detected as not uniform")),
+        pytest.param(TimeUnit.second, Uniformity.NOT_UNIFORM),
+        pytest.param(TimeUnit.minute, Uniformity.PERFECT),
+        pytest.param(TimeUnit.minute, Uniformity.UNIFORM, marks=pytest.mark.xfail(reason="for some reason, uniformity is detected as not uniform")),
+        pytest.param(TimeUnit.minute, Uniformity.NOT_UNIFORM),
+        *[(unit, uniformity) for uniformity in Uniformity for unit in TimeUnit if unit > TimeUnit.minute], #all units greater than minute pass all the tests
+    ]
+)
+def test_time_resolution_whole_pipeline(unit:TimeUnit, uniformity:Uniformity, num_rows=int(1e6)):
+    #generate some fake data
+    times = np.ones(num_rows,dtype=np.float64) * unit
+    times = times.cumsum()
+    times += np.random.randint(datetime(1000,1,1).timestamp(), datetime(3000,1,1).timestamp())
+
+    #remove any times more than the maximum datetime (year 9999)
+    times = times[times < datetime(9999,1,1).timestamp()]
+    num_rows = len(times)
+
+    if uniformity == Uniformity.PERFECT:
+        pass
+    elif uniformity == Uniformity.UNIFORM:
+        times += np.random.uniform(-0.004,0.004,num_rows)*unit
+    elif uniformity == Uniformity.NOT_UNIFORM:
+        times += np.random.uniform(-0.1,0.1,num_rows)*unit
+
+    #create a dataframe, with each time converted to a datetime string
+    dtimes = np.asarray(times, dtype='datetime64[s]').tolist()
+    df = pd.DataFrame({'date':dtimes})
+
+    #add latitude and longitude columns with random values
+    df['latitude'] = np.random.uniform(-90,90,num_rows)
+    df['longitude'] = np.random.uniform(-180,180,num_rows)
+
+    #add random feature columns
+    df['feat1'] = np.random.uniform(-100,100,num_rows)
+    df['feat2'] = np.random.uniform(-1,1,num_rows)
+    df['feat3'] = np.random.uniform(-1000,1000,num_rows)
+    df['feat4'] = np.random.uniform(-10000,10000,num_rows)
+
+    #save the dataframe to a csv
+    df.to_csv('test.csv',index=False)
+
+    #run geotime
+    t = geotime_classify.GeoTimeClassify(20)
+    res = t.columns_classified('test.csv')
+    if res is None:
+        raise Exception('geotime failed to classify the test data')
+
+    #check the time resolution
+    time_cols = [c for c in res.classifications if c.category == 'time']
     
+    #cleanup
+    os.remove('test.csv')
+    del df, res, t
 
-def time_resolution_whole_pipeline() -> TestGen:
-    def time_resolution_whole_pipeline(unit:TimeUnit, uniformity:Uniformity, num_rows=int(1e6)):
-        #generate some fake data
-        times = np.ones(num_rows,dtype=np.float64) * unit
-        times = times.cumsum()
-        times += np.random.randint(datetime(1000,1,1).timestamp(), datetime(3000,1,1).timestamp())
-
-        #remove any times more than the maximum datetime (year 9999)
-        times = times[times < datetime(9999,1,1).timestamp()]
-        num_rows = len(times)
-
-        if uniformity == Uniformity.PERFECT:
-            pass
-        elif uniformity == Uniformity.UNIFORM:
-            times += np.random.uniform(-0.0045,0.0045,num_rows)*unit
-        elif uniformity == Uniformity.NOT_UNIFORM:
-            times += np.random.uniform(-0.1,0.1,num_rows)*unit
-
-        #create a dataframe, with each time converted to a datetime string
-        dtimes = np.asarray(times, dtype='datetime64[s]').tolist()
-        df = pd.DataFrame({'date':dtimes})
-
-        #add latitude and longitude columns with random values
-        df['latitude'] = np.random.uniform(-90,90,num_rows)
-        df['longitude'] = np.random.uniform(-180,180,num_rows)
-
-        #add random feature columns
-        df['feat1'] = np.random.uniform(-100,100,num_rows)
-        df['feat2'] = np.random.uniform(-1,1,num_rows)
-        df['feat3'] = np.random.uniform(-1000,1000,num_rows)
-        df['feat4'] = np.random.uniform(-10000,10000,num_rows)
-
-        #save the dataframe to a csv
-        df.to_csv('test.csv',index=False)
-
-        #run geotime
-        t = geotime_classify.GeoTimeClassify(20)
-        res = t.columns_classified('test.csv')
-        if res is None:
-            raise Exception('geotime failed to classify the test data')
-
-        #check the time resolution
-        time_cols = [c for c in res.classifications if c.category == 'time']
-        assert len(time_cols) == 1, f'expected 1 time column, got {len(time_cols)}'
-        time_res = time_cols[0].time_resolution
-        assert time_res is not None, 'time resolution information was not detected'
-
-
-        assert time_res.unit == unit, f'failed to detect {unit}, instead got {time_res.unit}'
-        assert time_res.uniformity == uniformity, f'failed to detect {uniformity} uniformity for {unit}, instead got {time_res.uniformity}'
-
-        os.remove('test.csv')
-        del df, res, t
-
-
-    #generator for the test suite
-    for uniformity in Uniformity:
-        for unit in [*TimeUnit][1:]:
-            yield f'time_resolution_whole_pipeline({unit},{uniformity})', lambda: time_resolution_whole_pipeline(unit, uniformity)
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+    #tests
+    assert len(time_cols) == 1, f'expected 1 time column, got {len(time_cols)}'
+    time_res = time_cols[0].time_resolution
+    assert time_res is not None, 'time resolution information was not detected'
+    assert time_res.unit == unit, f'failed to detect {unit}, instead got {time_res.unit}'
+    assert time_res.uniformity == uniformity, f'failed to detect {uniformity} uniformity for {unit}, instead got {time_res.uniformity}'
