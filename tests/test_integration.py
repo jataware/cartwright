b98dfa58111512214d8186cb2ea861bcfd0f8069
David Samson <david.andrew.engineer@gmail.com>
2022-10-27 13:21:26 -0400

    some formatting updates

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,8 +2,15 @@ import pytest
 from cartwright.categorize import CartwrightClassify
 
 
-@pytest.mark.parametrize('filepath', [
-    pytest.param('examples/example_2.csv', marks=pytest.mark.xfail(reason='refactor has broken integration'))])
+@pytest.mark.parametrize('filepath',
+    [
+        pytest.param('examples/example_2.csv', marks=pytest.mark.xfail(reason='refactor has broken integration'))
+    ]
+)
 def test_simple_integration(filepath):
     t = CartwrightClassify(20)
     res = t.columns_classified(filepath)
+
+
+if __name__ == '__main__':
+    test_simple_integration('examples/example_2.csv')
