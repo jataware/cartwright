2564d24ea8540f9c2dab270f842064acd4b9915d
David Samson <david.andrew.engineer@gmail.com>
2022-11-09 09:58:19 -0500

    fix tox tests failing because of matplotlib import

--- tests/test_geospatial_resolution_detection.py
+++ tests/test_geospatial_resolution_detection.py
@@ -2,12 +2,35 @@ from typing import Tuple
 from scipy.stats import norm
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from cartwright.analysis.space_resolution import preprocess_latlon, detect_resolution, detect_latlon_resolution
 
 import pdb
 
 def main():
+
+    from matplotlib import pyplot as plt
+    def set_axes_equal(ax: plt.Axes):
+        """Set 3D plot axes to equal scale.
+
+        Make axes of 3D plot have equal scale so that spheres appear as
+        spheres and cubes as cubes.  Required since `ax.axis('equal')`
+        and `ax.set_aspect('equal')` don't work on 3D.
+        """
+        ax.set_box_aspect([1,1,1])
+        ax.set_proj_type('ortho')
+        limits = np.array([
+            ax.get_xlim3d(),
+            ax.get_ylim3d(),
+            ax.get_zlim3d(),
+        ])
+        origin = np.mean(limits, axis=1)
+        radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+        x, y, z = origin
+        ax.set_xlim3d([x - radius, x + radius])
+        ax.set_ylim3d([y - radius, y + radius])
+        ax.set_zlim3d([z - radius, z + radius])
+
+
 
     #Some experiments with plotting points on a sphere
     n_points = 500
@@ -168,26 +191,6 @@ def test1():
 
 
 
-def set_axes_equal(ax: plt.Axes):
-    """Set 3D plot axes to equal scale.
-
-    Make axes of 3D plot have equal scale so that spheres appear as
-    spheres and cubes as cubes.  Required since `ax.axis('equal')`
-    and `ax.set_aspect('equal')` don't work on 3D.
-    """
-    ax.set_box_aspect([1,1,1])
-    ax.set_proj_type('ortho')
-    limits = np.array([
-        ax.get_xlim3d(),
-        ax.get_ylim3d(),
-        ax.get_zlim3d(),
-    ])
-    origin = np.mean(limits, axis=1)
-    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
-    x, y, z = origin
-    ax.set_xlim3d([x - radius, x + radius])
-    ax.set_ylim3d([y - radius, y + radius])
-    ax.set_zlim3d([z - radius, z + radius])
 
 
 
